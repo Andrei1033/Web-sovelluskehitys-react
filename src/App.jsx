25bc5d34f96efd4ca4c11f598cc9a14e55fc995d
andre <andrei.tsizikov00@gmail.com>
2026-04-12 19:58:43 +0300

    First components exercise completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import './App.css';
+import Home from '../src/components/home.jsx';
+
 const App = () => {
   return (
     <>
       <h1>My Appp</h1>
+      <Home />
     </>
   );
 };
---
+++ src/components/home.jsx
@@ -0,0 +1,64 @@
+import MediaRow from "./MediaRow";
+
+const Home = () => {
+    const mediaArray = [
+        {
+            media_id: 8,
+            user_id: 5,
+            filename: 'https://placehold.co/1200x800',
+            thumbnail: 'https://placehold.co/320x240',
+            filesize: 170469,
+            media_type: 'image/jpeg',
+            title: 'Picture 1',
+            description: 'This is a placeholder picture.',
+            created_at: '2024-01-07T20:49:34.000Z',
+        },
+        {
+            media_id: 9,
+            user_id: 7,
+            filename: 'https://placehold.co/1200x800',
+            thumbnail: 'https://placehold.co/320x240',
+            media_type: 'image/jpeg',
+            title: 'Pic 2',
+            description: '',
+            created_at: '2024-01-07T21:32:27.000Z',
+        },
+        {
+            media_id: 17,
+            user_id: 2,
+            filename: 'https://placehold.co/1200x800',
+            thumbnail: 'https://placehold.co/320x240',
+            filesize: 1236616,
+            media_type: 'video/mp4',
+            title: 'Bunny',
+            description: 'Butterflies fly around the bunny.',
+            created_at: '2024-01-07T20:48:13.000Z',
+        },
+    ]
+    return (
+        <>
+            <h2>My Media</h2>
+
+            <table>
+                <thead>
+                <tr>
+                    <th>Thumbnail</th>
+                    <th>Title</th>
+                    <th>Description</th>
+                    <th>Created</th>
+                    <th>Size</th>
+                    <th>Type</th>
+                </tr>
+                </thead>
+
+                <tbody>
+                {mediaArray.map((item) => (
+                    <MediaRow key={item.media_id} item={item}/>
+                ))}
+                </tbody>
+            </table>
+        </>
+    );
+};
+
+export default Home;
