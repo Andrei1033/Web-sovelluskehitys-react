219e038412bcef9b4a4bf0e69f2f346bba3a4f53
andre <andrei.tsizikov00@gmail.com>
2026-04-14 20:26:12 +0300

    hooks Lab assignment 1

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,44 +1,26 @@
 import MediaRow from "./MediaRow";
-import { useState } from "react";
+import { useState, useEffect} from "react";
+import fetchData from '../utils/fetchData';
 
 const Home = () => {
-    const mediaArray = [
-        {
-            media_id: 8,
-            user_id: 5,
-            filename: 'https://plus.unsplash.com/premium_photo-1687411984360-9560bc31048f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1M3x8fGVufDB8fHx8fA%3D%3D',
-            thumbnail: 'https://plus.unsplash.com/premium_photo-1687411984360-9560bc31048f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1M3x8fGVufDB8fHx8fA%3D%3D',
-            filesize: 170469,
-            media_type: 'mp4',
-            title: 'Vid Vid VVVVVid 1',
-            description: 'This is a placeholder video.',
-            created_at: '2024-01-07T20:49:34.000Z',
-        },
-        {
-            media_id: 9,
-            user_id: 7,
-            filename: 'https://images.unsplash.com/photo-1775840532502-59540d252c54?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8Ym84alFLVGFFMFl8fGVufDB8fHx8fA%3D%3D',
-            thumbnail: 'https://images.unsplash.com/photo-1775840532502-59540d252c54?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8Ym84alFLVGFFMFl8fGVufDB8fHx8fA%3D%3D',
-            media_type: 'image/jpeg',
-            title: 'Pic 2',
-            description: '',
-            created_at: '2024-01-07T21:32:27.000Z',
-        },
-        {
-            media_id: 17,
-            user_id: 2,
-            filename: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWSr0OfFPxOcTqm5pjU_3wySt04sHbcREbOg&s',
-            thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWSr0OfFPxOcTqm5pjU_3wySt04sHbcREbOg&s',
-            filesize: 1236616,
-            media_type: 'video/mp4',
-            title: 'Bunny',
-            description: 'Butterflies fly around the bunny.',
-            created_at: '2024-01-07T20:48:13.000Z',
-        },
-    ]
-
+    const [mediaArray, setMediaArray] = useState([]);
     const [search, setSearch] = useState('');
     const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        const getMedia = async () =>{
+            try{
+                const json = await fetchData("test.json");
+                setMediaArray(json);
+            }
+            catch (error){
+                console.error(error);
+            }
+        };
+        getMedia();
+    }, []);
+    
+
     const filteredMedia = mediaArray.filter((item) =>
         item.title.toLowerCase().includes(search.toLowerCase())
     );
